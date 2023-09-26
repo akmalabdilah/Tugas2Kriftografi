ef43f6f6e1c0c0fa11dc13f3dbdd4194b87c9eb0
M. AKMAL AL ABDILAH <92352562+akmalabdilah@users.noreply.github.com>
2023-09-26 14:08:57 +0700

    menambahkan file

---
+++ vigenere chiper/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Selamat Datang</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h2>Selamat Datang</h2>
+    <div class="menu">
+        <a href="login.php">Login</a>
+        <a href="register.php">Registrasi</a>
+    </div>
+</body>
+</html>
